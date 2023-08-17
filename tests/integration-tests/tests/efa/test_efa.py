08b69e1445c6158b8e323f1078415aad4e0dbc6b
Himani Deshpande <himanidp@amazon.com>
2023-08-17 10:47:57 -0400

    Run NCCL Benchmarks and MPI JOB Last

--- tests/integration-tests/tests/efa/test_efa.py
+++ tests/integration-tests/tests/efa/test_efa.py
@@ -62,7 +62,11 @@ def test_efa(
     scheduler_commands = scheduler_commands_factory(remote_command_executor)
 
     _test_efa_installation(scheduler_commands, remote_command_executor, efa_installed=True, partition="efa-enabled")
-    # _test_mpi(remote_command_executor, slots_per_instance, scheduler, scheduler_commands, partition="efa-enabled")
+
+    if instance in ["p4d.24xlarge", "p5.48xlarge"] and os != "centos7":
+        _test_nccl_benchmarks(remote_command_executor, test_datadir, "openmpi", scheduler_commands)
+
+
     logging.info("Running on Instances: {0}".format(get_compute_nodes_instance_ids(cluster.cfn_name, region)))
 
     # run_system_analyzer(cluster, scheduler_commands_factory, request, partition="efa-enabled")
@@ -109,8 +113,7 @@ def test_efa(
         )
     _test_shm_transfer_is_enabled(scheduler_commands, remote_command_executor, partition="efa-enabled")
 
-    if instance == "p4d.24xlarge" and os != "centos7":
-        _test_nccl_benchmarks(remote_command_executor, test_datadir, "openmpi", scheduler_commands)
+    _test_mpi(remote_command_executor, slots_per_instance, scheduler, scheduler_commands, partition="efa-enabled")
 
     assert_no_errors_in_logs(remote_command_executor, scheduler, skip_ice=True)
 
