ff2883e4d2d2d3d35fbe6918916a9b65ecb0181e
hanwenli <hanwenli@amazon.com>
2025-08-26 12:43:13 -0700

    Add EFA checks to GB200

---
+++ tests/integration-tests/tests/ultraserver/test_gb200/test_gb200/nccl_benchmarks/init_nccl_benchmarks.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+exec &>/shared/init_nccl.out
+set -xe
+
+rm -rf /shared/${1}
+
+module load ${1}
+NCCL_BENCHMARKS_VERSION='2.16.7'
+NCCL_VERSION='2.27.7-1'
+MPI_HOME=$(which mpirun | awk -F '/bin' '{print $1}')
+NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_90,code=sm_90 -gencode=arch=compute_90,code=compute_90" # Arch for NVIDIA A100 and H100, ref https://docs.nvidia.com/cuda/ada-compatibility-guide/index.html
+
+mkdir -p /shared/${1}
+
+# Install and build nccl from sources
+cd /shared/${1}
+wget https://github.com/NVIDIA/nccl/archive/v${NCCL_VERSION}.tar.gz
+tar zxvf "v${NCCL_VERSION}.tar.gz"
+cd nccl-${NCCL_VERSION}
+make -j src.build NVCC_GENCODE="${NVCC_GENCODE}"
+
+# Build nccl-tests
+cd /shared/${1}
+wget https://github.com/NVIDIA/nccl-tests/archive/v${NCCL_BENCHMARKS_VERSION}.tar.gz
+tar zxvf "v${NCCL_BENCHMARKS_VERSION}.tar.gz"
+cd "nccl-tests-${NCCL_BENCHMARKS_VERSION}/"
+NVCC_GENCODE="${NVCC_GENCODE}" make MPI=1 MPI_HOME=${MPI_HOME} NCCL_HOME=/shared/${1}/nccl-${NCCL_VERSION}/build/ CUDA_HOME=/usr/local/cuda
