566946bcd981c4d422c83b339093d25716a3ceb3
Himani Anil Deshpande <himanidp@amazon.com>
2025-08-27 23:01:24 -0400

    Remove skip_ice

--- tests/integration-tests/tests/ultraserver/test_gb200.py
+++ tests/integration-tests/tests/ultraserver/test_gb200.py
@@ -389,6 +389,7 @@ def test_gb200(
     This is a reasonable approximation for the test because the focus of the test is on IMEX and topology configuration,
     which can be executed on g4dn as well.
     """
+    time.sleep(10*60)
     max_queue_size = 2
     min_queue_size_without_imex = 1 if instance != "p6e-gb200.36xlarge" else 0
     capacity_block_reservation_id = CAPACITY_BLOCK_RESERVATION_ID if instance == "p6e-gb200.36xlarge" else None
@@ -471,48 +472,45 @@ def test_gb200(
 
     # Test that IMEX and topology are not configured for queue without IMEX support
     with soft_assertions():
-        # We enable nvidia-imex force_configuration only for non-gb200 instances
+        # We enable nvidia-imex force_configuration only for non-gb200 instances, so we should assert these checks with same condition
         if instance != "p6e-gb200.36xlarge":
             assert_imex_not_configured(cluster, queue_without_imex, compute_resource_without_imex)
         # Topology Plugin is Cluster wide setup so we check if compute_resource_without_imex is not in that file
         assert_topology_plugin_not_configured_for_queue(cluster, queue_without_imex, compute_resource_without_imex)
 
     # Test cluster update with changed topology configuration
-    if instance == "p6e-gb200.36xlarge":
-        # The size of Capacity Block remains constant
-        max_queue_size_updated = max_queue_size
-    else:
-        max_queue_size_updated = 3
-        updated_cluster_config = pcluster_config_reader(
-            config_file="pcluster.config.update.yaml",
-            bucket_name=bucket_name,
-            head_node_start_script=headnode_start_filename,
-            min_queue_size_without_imex=min_queue_size_without_imex,
-            max_queue_size=max_queue_size_updated,
-            queue_with_imex=queue_with_imex,
-            compute_resource_with_imex=compute_resource_with_imex,
-            queue_without_imex=queue_without_imex,
-            compute_resource_without_imex=compute_resource_without_imex,
-        )
+    max_queue_size_updated = 3
+    updated_cluster_config = pcluster_config_reader(
+        config_file="pcluster.config.update.yaml",
+        bucket_name=bucket_name,
+        head_node_start_script=headnode_start_filename,
+        max_queue_size_without_imex=max_queue_size_without_imex,
+        max_queue_size=max_queue_size_updated,
+        queue_with_imex=queue_with_imex,
+        compute_resource_with_imex=compute_resource_with_imex,
+        queue_without_imex=queue_without_imex,
+        compute_resource_without_imex=compute_resource_without_imex,
+    )
 
-        cluster.stop()
-        wait_for_computefleet_changed(cluster, "STOPPED")
-        cluster.update(str(updated_cluster_config), force_update=True)
-        cluster.start()
-        wait_for_computefleet_changed(cluster, "RUNNING")
-        # Wait for compute nodes to be fully running
-        wait_for_instances_in_compute_resource(
-            cluster, queue_with_imex, compute_resource_with_imex, ["running"], max_queue_size_updated
-        )
+    cluster.stop()
+    wait_for_computefleet_changed(cluster, "STOPPED")
+    cluster.update(str(updated_cluster_config), force_update=True)
+    cluster.start()
+    wait_for_computefleet_changed(cluster, "RUNNING")
+    # Wait for compute nodes to be fully running
+    wait_for_instances_in_compute_resource(
+        cluster, queue_with_imex, compute_resource_with_imex, ["running"], max_queue_size_updated
+    )
 
-        # Verify imex and topology plugin configuration after update
-        assert_imex_healthy(cluster, queue_with_imex, compute_resource_with_imex, max_queue_size_updated)
-        assert_topology_plugin_configured(
-            cluster, queue_with_imex, compute_resource_with_imex, f"{max_queue_size_updated}", max_queue_size_updated
-        )
-        with soft_assertions():
+    # Verify imex and topology plugin configuration after update
+    assert_imex_healthy(cluster, queue_with_imex, compute_resource_with_imex, max_queue_size_updated)
+    assert_topology_plugin_configured(
+        cluster, queue_with_imex, compute_resource_with_imex, f"{max_queue_size_updated}", max_queue_size_updated
+    )
+    with soft_assertions():
+        if instance != "p6e-gb200.36xlarge":
             assert_imex_not_configured(cluster, queue_without_imex, compute_resource_without_imex)
-            assert_topology_plugin_not_configured_for_queue(cluster, queue_without_imex, compute_resource_without_imex)
+        assert_topology_plugin_not_configured_for_queue(cluster, queue_without_imex, compute_resource_without_imex)
 
     # Forcefully terminate a compute node in the compute resource supporting IMEX
     # to simulate an outage that forces the replacement of the node and consequently the IMEX reconfiguration.
@@ -527,14 +525,13 @@ def test_gb200(
     # Verify IMEX is still healthy after node replacement
     assert_imex_healthy(cluster, queue_with_imex, compute_resource_with_imex, max_queue_size_updated)
 
-    min_queue_size_without_imex = 0  # Update MinCount to 0 so that the slurmctld can be restarted.
     # Test final cluster update to remove topology plugin configuration completely
     final_cluster_config = pcluster_config_reader(
         config_file="pcluster.config.final.yaml",
         bucket_name=bucket_name,
         head_node_start_script=headnode_start_filename,
         max_queue_size=max_queue_size_updated,
-        min_queue_size_without_imex=min_queue_size_without_imex,
+        max_queue_size_without_imex=max_queue_size_without_imex,
         queue_with_imex=queue_with_imex,
         compute_resource_with_imex=compute_resource_with_imex,
         queue_without_imex=queue_without_imex,
@@ -547,12 +544,153 @@ def test_gb200(
     cluster.start()
     wait_for_computefleet_changed(cluster, "RUNNING")
 
-    # Verify topology plugin is completely disabled after removing force_configuration if using non-nvl instances
-    # Verify topology plugin is completely disabled after removing the queue which contains nvl instances
-    assert_topology_plugin_completely_disabled(cluster)
+    # Verify topology plugin is completely disabled after removing force_configuration
+    if instance != "p6e-gb200.36xlarge":
+        assert_topology_plugin_completely_disabled(cluster)
 
     # Verify IMEX still works but topology is completely removed
+    assert_imex_healthy(cluster, queue_with_imex, compute_resource_with_imex, max_queue_size_updated)
     with soft_assertions():
         if instance != "p6e-gb200.36xlarge":
-            assert_imex_healthy(cluster, queue_with_imex, compute_resource_with_imex, max_queue_size_updated)
             assert_imex_not_configured(cluster, queue_without_imex, compute_resource_without_imex)
+
+
+def _execute_fabtests(remote_command_executor, test_datadir, instance):
+    fabtests_dir = "/shared/fabtests"
+    fabtests_pid_file = f"{fabtests_dir}/outputs/fabtests.pid"
+    fabtests_log_file = f"{fabtests_dir}/outputs/fabtests.log"
+    fabtests_report_file = f"{fabtests_dir}/outputs/fabtests.report"
+
+    logging.info("Installing Fabtests")
+    remote_command_executor.run_remote_script(
+        str(test_datadir / "install-fabtests.sh"), args=[fabtests_dir], timeout=600
+    )
+
+    logging.info("Running Fabtests")
+    test_cases = FABTESTS_BASIC_TESTS + FABTESTS_GDRCOPY_TESTS if instance == "p4d.24xlarge" else FABTESTS_BASIC_TESTS
+
+    if "g6" in instance:
+        test_cases = test_cases + ["not cuda"]
+
+    remote_command_executor.run_remote_script(
+        str(test_datadir / "run-fabtests.sh"),
+        args=[
+            fabtests_dir,
+            fabtests_pid_file,
+            fabtests_log_file,
+            fabtests_report_file,
+            "efa-enabled-st-efa-enabled-i1-1",
+            "efa-enabled-st-efa-enabled-i1-2",
+            ",".join(test_cases),
+            "enable-gdr" if instance == "p4d.24xlarge" else "skip-gdr",
+        ],
+        timeout=60,
+        pty=False,
+    )
+
+    pid = read_remote_file(remote_command_executor, fabtests_pid_file)
+
+    wait_process_completion(remote_command_executor, pid)
+
+    logging.info("Retrieving Fabtests report")
+    report_content = read_remote_file(remote_command_executor, fabtests_report_file)
+    logging.info("Parsing Fabtests report")
+    return xmltodict.parse(report_content)
+
+
+def _test_efa_installation(scheduler_commands, remote_command_executor, efa_installed=True, partition=None):
+    # Output contains:
+    # 00:06.0 Ethernet controller: Amazon.com, Inc. Device efa0
+    logging.info("Testing EFA installed")
+    if partition:
+        result = scheduler_commands.submit_command("lspci -n > /shared/lspci.out", partition=partition)
+    else:
+        result = scheduler_commands.submit_command("lspci -n > /shared/lspci.out")
+
+    job_id = scheduler_commands.assert_job_submitted(result.stdout)
+    scheduler_commands.wait_job_completed(job_id)
+    scheduler_commands.assert_job_succeeded(job_id)
+
+    # Check if EFA interface is on compute node
+    result = remote_command_executor.run_remote_command("cat /shared/lspci.out")
+    if efa_installed:
+        assert_that(result.stdout).contains("1d0f:efa")
+    else:
+        assert_that(result.stdout).does_not_contain("1d0f:efa")
+
+    # Check EFA interface not present on head node
+    result = remote_command_executor.run_remote_command("lspci -n")
+    assert_that(result.stdout).does_not_contain("1d0f:efa")
+
+
+def _test_shm_transfer_is_enabled(scheduler_commands, remote_command_executor, partition=None):
+    logging.info("Testing SHM Transfer is enabled")
+    if partition:
+        result = scheduler_commands.submit_command("fi_info -p efa 2>&1 > /shared/fi_info.out", partition=partition)
+    else:
+        result = scheduler_commands.submit_command("fi_info -p efa 2>&1 > /shared/fi_info.out")
+    job_id = scheduler_commands.assert_job_submitted(result.stdout)
+    scheduler_commands.wait_job_completed(job_id)
+    job_stdout = remote_command_executor.run_remote_command(f"cat slurm-{job_id}.out").stdout
+    logging.info(f"Job stdout is: {job_stdout}")
+    scheduler_commands.assert_job_succeeded(job_id)
+    result = remote_command_executor.run_remote_command("cat /shared/fi_info.out")
+    assert_that(result.stdout).does_not_contain("SHM transfer will be disabled because of ptrace protection")
+
+
+def _test_nccl_benchmarks(remote_command_executor, test_datadir, mpi_module, scheduler_commands, instance):
+    logging.info("Running NCCL benchmarks")
+    remote_command_executor.run_remote_script(
+        str(test_datadir / "nccl_benchmarks" / "init_nccl_benchmarks.sh"), args=[mpi_module], hide=True, timeout=600
+    )
+
+    result = scheduler_commands.submit_script(
+        str(test_datadir / "nccl_benchmarks" / "nccl_tests_submit_{0}.sh".format(mpi_module)), nodes=2
+    )
+
+    job_id = scheduler_commands.assert_job_submitted(result.stdout)
+    scheduler_commands.wait_job_completed(job_id)
+    scheduler_commands.assert_job_succeeded(job_id)
+
+    result = remote_command_executor.run_remote_command("cat /shared/nccl_tests.out")
+    logging.info(f"Test result is: {result}")
+
+    # Expected output with NCCL_BENCHMARKS_VERSION='2.10.0', NCCL_VERSION='2.7.8-1' and OFI_NCCL_VERSION='1.1.1':
+    #                                                       out-of-place                       in-place
+    #       size         count      type   redop     time   algbw   busbw  error     time   algbw   busbw  error
+    #        (B)    (elements)                       (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
+    # ...
+    # 1073741824     268435456     float     sum    79531   13.50   26.58  2e-06    79371   13.53   26.63  2e-06
+    #
+    # --------
+    # Expected output with NCCL_BENCHMARKS_VERSION='2.13.8', NCCL_VERSION='2.19.4-1' and OFI_NCCL_VERSION='1.7.4-aws':
+    #                                                              out-of-place                       in-place
+    #       size         count      type   redop    root     time   algbw   busbw #wrong     time   algbw   busbw #wrong
+    #        (B)    (elements)                               (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
+    # ...
+    # 1073741824     268435456     float     sum      -1    44023   24.39   45.73      0    43947   24.43   45.81      0
+
+    # We are looking for packet size 1073741824, 268435456 elements and in-place busbw (GB/s).
+    max_bandwidth = remote_command_executor.run_remote_command(
+        "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $12}'"
+    ).stdout
+
+    out_of_place_max_bandwidth = remote_command_executor.run_remote_command(
+        "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $8}'"
+    ).stdout
+
+    instance_bandwidth_dict = {
+        # p4d.24xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 27GB/s
+        "p4d.24xlarge": 26.0,
+        # p5.48xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 250GB/s
+        "p5.48xlarge": 250.0,
+        "p6e-gb200.36xlarge": 500
+    }
+
+    expected_bandwidth = instance_bandwidth_dict.get(instance)
+    if expected_bandwidth is None:
+        pytest.fail(f"Instance {instance} is not valid for multiple bandwidth tests")
+
+    assert_that(float(max_bandwidth)).is_greater_than(expected_bandwidth)
+    if instance=="p6e-gb200.36xlarge":
+        assert_that(float(out_of_place_max_bandwidth)).is_greater_than(expected_bandwidth)
