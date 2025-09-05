794ef59129cc287a4ac3a710a6f3f3b3eea31a50
Himani Anil Deshpande <himanidp@amazon.com>
2025-09-05 11:51:07 -0400

    Submit a burst of NCCL Jobs with different gpu values

--- tests/integration-tests/tests/ultraserver/test_gb200.py
+++ tests/integration-tests/tests/ultraserver/test_gb200.py
@@ -23,7 +23,7 @@ from clusters_factory import Cluster
 from remote_command_executor import RemoteCommandExecutor
 from tests.common.mpi_common import _test_mpi
 from tests.efa.test_efa import FABTESTS_BASIC_TESTS, FABTESTS_GDRCOPY_TESTS
-from utils import wait_for_computefleet_changed, get_compute_nodes_instance_ids
+from utils import wait_for_computefleet_changed, get_compute_nodes_instance_ids, get_instance_info
 from retrying import retry
 
 from tests.common.assertions import assert_regex_in_file, wait_for_instances_in_compute_resource
@@ -461,7 +461,7 @@ def test_gb200(
     #     assert_that(num_errors, description=f"{num_errors}/{num_tests} libfabric tests got errors").is_equal_to(0)
     #     assert_no_errors_in_logs(remote_command_executor, scheduler, skip_ice=True)
 
-
+    #want to Run Burst of NCCL Jobs
     _test_nccl_benchmarks(remote_command_executor, test_datadir, "openmpi", scheduler_commands, instance)
 
     # Test IMEX and topology configuration for queue with IMEX support
@@ -638,59 +638,82 @@ def _test_shm_transfer_is_enabled(scheduler_commands, remote_command_executor, p
     assert_that(result.stdout).does_not_contain("SHM transfer will be disabled because of ptrace protection")
 
 
+
 def _test_nccl_benchmarks(remote_command_executor, test_datadir, mpi_module, scheduler_commands, instance):
     logging.info("Running NCCL benchmarks")
     remote_command_executor.run_remote_script(
         str(test_datadir / "nccl_benchmarks" / "init_nccl_benchmarks.sh"), args=[mpi_module], hide=True, timeout=600
     )
+    #want to Run Burst of NCCL Jobs
+    list_of_jobs = []
+    for gpu_per_node in ['2','2','4','4']:
+    # try:
+    #     gpu_per_node = get_instance_info(instance)["GpuInfo"]["Gpus"][0]["Count"]
+    # except Exception as e:
+    #     # Getting information from us-east-1 is useful for p6e-GB200
+    #     # because it is only publicly available in us-east-1 while we are testing in other regions.
+    #     logging.warning(
+    #         f"Failed to get gpu count for {instance} from the current region. Exception: {e}. "
+    #         "Trying to retrieve the information from us-east-1..."
+    #     )
+    #     gpu_per_node = get_instance_info(instance, "us-east-1")["GpuInfo"]["Gpus"][0]["Count"]
 
-    result = scheduler_commands.submit_script(
-        str(test_datadir / "nccl_benchmarks" / "nccl_tests_submit_{0}.sh".format(mpi_module)), nodes=2
-    )
+        result = scheduler_commands.submit_script(
+            str(test_datadir / "nccl_benchmarks" / "nccl_tests_submit_{0}.sh".format(mpi_module)),
+            nodes=2,
+            ntasks_per_node=gpu_per_node,
+        )
 
-    job_id = scheduler_commands.assert_job_submitted(result.stdout)
-    scheduler_commands.wait_job_completed(job_id)
-    scheduler_commands.assert_job_succeeded(job_id)
+        job_id = scheduler_commands.assert_job_submitted(result.stdout)
+        list_of_jobs.append(job_id)
 
-    result = remote_command_executor.run_remote_command("cat /shared/nccl_tests.out")
-    logging.info(f"Test result is: {result}")
 
-    # Expected output with NCCL_BENCHMARKS_VERSION='2.10.0', NCCL_VERSION='2.7.8-1' and OFI_NCCL_VERSION='1.1.1':
-    #                                                       out-of-place                       in-place
-    #       size         count      type   redop     time   algbw   busbw  error     time   algbw   busbw  error
-    #        (B)    (elements)                       (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
-    # ...
-    # 1073741824     268435456     float     sum    79531   13.50   26.58  2e-06    79371   13.53   26.63  2e-06
-    #
-    # --------
-    # Expected output with NCCL_BENCHMARKS_VERSION='2.13.8', NCCL_VERSION='2.19.4-1' and OFI_NCCL_VERSION='1.7.4-aws':
-    #                                                              out-of-place                       in-place
-    #       size         count      type   redop    root     time   algbw   busbw #wrong     time   algbw   busbw #wrong
-    #        (B)    (elements)                               (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
-    # ...
-    # 1073741824     268435456     float     sum      -1    44023   24.39   45.73      0    43947   24.43   45.81      0
+    for job_id in list_of_jobs:
+        scheduler_commands.wait_job_completed(job_id)
+        scheduler_commands.assert_job_succeeded(job_id)
 
-    # We are looking for packet size 1073741824, 268435456 elements and in-place busbw (GB/s).
-    max_bandwidth = remote_command_executor.run_remote_command(
-        "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $12}'"
-    ).stdout
+        result = remote_command_executor.run_remote_command("cat /shared/nccl_tests.out")
+        logging.info(f"Test result is: {result}")
 
-    out_of_place_max_bandwidth = remote_command_executor.run_remote_command(
-        "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $8}'"
-    ).stdout
+        # Expected output with NCCL_BENCHMARKS_VERSION='2.10.0', NCCL_VERSION='2.7.8-1' and OFI_NCCL_VERSION='1.1.1':
+        #                                                       out-of-place                       in-place
+        #       size         count      type   redop     time   algbw   busbw  error     time   algbw   busbw  error
+        #        (B)    (elements)                       (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
+        # ...
+        # 1073741824     268435456     float     sum    79531   13.50   26.58  2e-06    79371   13.53   26.63  2e-06
+        #
+        # --------
+        # Expected output with NCCL_BENCHMARKS_VERSION='2.13.8', NCCL_VERSION='2.19.4-1' and OFI_NCCL_VERSION='1.7.4-aws':
+        #                                                              out-of-place                       in-place
+        #       size         count      type   redop    root     time   algbw   busbw #wrong     time   algbw   busbw #wrong
+        #        (B)    (elements)                               (us)  (GB/s)  (GB/s)            (us)  (GB/s)  (GB/s)
+        # ...
+        # 1073741824     268435456     float     sum      -1    44023   24.39   45.73      0    43947   24.43   45.81      0
 
-    instance_bandwidth_dict = {
-        # p4d.24xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 27GB/s
-        "p4d.24xlarge": 26.0,
-        # p5.48xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 250GB/s
-        "p5.48xlarge": 250.0,
-        "p6e-gb200.36xlarge": 500
-    }
+        # We are looking for packet size 1073741824, 268435456 elements and in-place busbw (GB/s).
+        max_bandwidth = remote_command_executor.run_remote_command(
+            "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $12}'"
+        ).stdout
 
-    expected_bandwidth = instance_bandwidth_dict.get(instance)
-    if expected_bandwidth is None:
-        pytest.fail(f"Instance {instance} is not valid for multiple bandwidth tests")
+        out_of_place_max_bandwidth = remote_command_executor.run_remote_command(
+            "cat /shared/nccl_tests.out | grep -E '1073741824\\s+268435456' | awk '{print $8}'"
+        ).stdout
 
-    assert_that(float(max_bandwidth)).is_greater_than(expected_bandwidth)
-    if instance=="p6e-gb200.36xlarge":
-        assert_that(float(out_of_place_max_bandwidth)).is_greater_than(expected_bandwidth)
+        instance_bandwidth_dict = {
+            # p4d.24xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 27GB/s
+            "p4d.24xlarge": 26.0,
+            # p5.48xlarge - Expected "in-place busbw" bandwidth with 2 nodes, 8 tasks per node is about 250GB/s
+            "p5.48xlarge": 250.0,
+            "p6e-gb200.36xlarge": 500,
+        }
+
+        expected_bandwidth = instance_bandwidth_dict.get(instance)
+        if expected_bandwidth is None:
+            pytest.fail(f"Instance {instance} is not valid for multiple bandwidth tests")
+
+        assert_that(float(max_bandwidth)).is_greater_than(expected_bandwidth)
+        if instance == "p6e-gb200.36xlarge":
+            # Check "out of place" bandwidth for p6e-GB200
+            # because the GPUs are directly connected for different instances on the same ultra server.
+            # The "out of place" bandwidth is expected to be similar to the in-place bandwidth.
+            assert_that(float(out_of_place_max_bandwidth)).is_greater_than(expected_bandwidth)
