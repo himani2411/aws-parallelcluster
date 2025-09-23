ec28f270279ca1df53339836934199cf1688c0ee
Himani Anil Deshpande <himanidp@amazon.com>
2025-09-23 16:59:50 -0400

    [Gb200][NCCL][Rocky] Revert to old test when NCCL job passed for rocky9

--- tests/integration-tests/tests/common/data/nccl/nccl_tests_submit_openmpi.sh
+++ tests/integration-tests/tests/common/data/nccl/nccl_tests_submit_openmpi.sh
@@ -23,10 +23,12 @@ fi
 # -x FI_EFA_USE_DEVICE_RDMA=1 is not needed from aws nccl pfi plugin version v1.6.0
 # -x NCCL_SOCKET_FAMILY=AF_INET forces NCCL to use IPv4. On Ubuntu 24 with p6-b200, without this parameter, NCCL hangs on IPv6, which is not supported by ParallelCluster
 mpirun \
+-x FI_PROVIDER="efa" \
+-x FI_EFA_USE_DEVICE_RDMA=1 \
 -x LD_LIBRARY_PATH=/shared/openmpi/nccl-${NCCL_VERSION}/build/lib/:${OFI_PATH}:$LD_LIBRARY_PATH \
--x NCCL_DEBUG=WARNING \
--x NCCL_TESTS_SPLIT_MASK=0x0 \
--x NCCL_SOCKET_FAMILY=AF_INET \
 -x RDMAV_FORK_SAFE=1 \
---bind-to none \
-/shared/openmpi/nccl-tests-${NCCL_BENCHMARKS_VERSION}/build/all_reduce_perf -b 1024 -e 8G -f 2 -g 1 -c 1 > /shared/nccl_tests.out
+-x NCCL_ALGO=ring \
+-x NCCL_DEBUG=WARNING \
+-x NCCL_PROTO=simple \
+--mca pml ^cm --mca btl tcp,self --mca btl_tcp_if_exclude lo,docker0 --bind-to none \
+/shared/openmpi/nccl-tests-${NCCL_BENCHMARKS_VERSION}/build/all_reduce_perf -b 8 -e 1G -f 2 -g 1 -c 1 -n 100 > /shared/nccl_tests.out
