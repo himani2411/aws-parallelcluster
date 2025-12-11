622b8b5e775c4c24ac37bce03c3d790846dffdb1
Himani Anil Deshpande <himanidp@amazon.com>
2025-12-11 17:46:40 -0500

    Run jobs with proper output files

--- tests/integration-tests/tests/common/data/osu/osu_pt2pt_submit_intelmpi.sh
+++ tests/integration-tests/tests/common/data/osu/osu_pt2pt_submit_intelmpi.sh
@@ -9,4 +9,4 @@ export I_MPI_DEBUG=10
 
 env
 
-mpirun -bootstrap=slurm -np 2 -ppn 2 -genv I_MPI_SHM=0 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=efa /shared/intelmpi/osu-micro-benchmarks-${OSU_BENCHMARK_VERSION}/mpi/pt2pt/${BENCHMARK_NAME} > /shared/${BENCHMARK_NAME}.out
+mpirun -bootstrap=slurm -np 2 -ppn 2 -genv I_MPI_SHM=0 -genv I_MPI_FABRICS=ofi -genv I_MPI_OFI_PROVIDER=efa /shared/intelmpi/osu-micro-benchmarks-${OSU_BENCHMARK_VERSION}/mpi/pt2pt/${BENCHMARK_NAME} > /shared/${BENCHMARK_NAME}_${SLURM_JOB_ID}_${SLURMD_NODENAME}.out
