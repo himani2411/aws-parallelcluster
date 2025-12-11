c69a6427508135d8da7a5d3f971959155b95b66a
Himani Anil Deshpande <himanidp@amazon.com>
2025-12-11 16:04:07 -0500

    Using same HN as Compute node

--- tests/integration-tests/tests/efa/test_efa.py
+++ tests/integration-tests/tests/efa/test_efa.py
@@ -51,7 +51,7 @@ def test_efa(
     max_queue_size = 32 if instance in osu_benchmarks_instances else 2
 
     if architecture == "x86_64":
-        head_node_instance = "c5.18xlarge"
+        head_node_instance = "c5n.18xlarge"
     else:
         head_node_instance = "c6g.16xlarge"
 
